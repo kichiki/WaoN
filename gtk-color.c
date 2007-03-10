8bac61da2a10e77d7c0889307038e4fffe9c35dd
kengo ichiki <kengoichiki@gmail.com>
2007-03-10 20:40:31 +0000

    (get_color): using not the pointer but GdkColor itself.

--- gtk-color.c
+++ gtk-color.c
@@ -1,6 +1,6 @@
 /* gtk-color.c
  * Copyright (C) 1999,2003,2007 Kengo Ichiki <kichiki@kichiki.com>
- * $Id: gtk-color.c,v 1.1 2007/02/09 23:03:59 kichiki Exp $
+ * $Id: gtk-color.c,v 1.2 2007/03/10 20:40:31 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-#include <stdlib.h> /* malloc() */
 #include <math.h> /* log10() */
 #include <gtk/gtk.h>
 
@@ -30,32 +29,29 @@
 void
 get_color (GtkWidget *widget, gint red, gint green, gint blue, GdkGC *gc)
 {
-  GdkColor *color;
+  GdkColor color;
 
-  /* the color we want to use */
-  color = (GdkColor *) malloc (sizeof (GdkColor));
-  
   /* red, green, and blue are passed values, indicating the RGB triple
    * of the color we want to draw. Note that the values of the RGB components
    * within the GdkColor are taken from 0 to 65535, not 0 to 255.
    */
-  color->red = red * (65535/255);
-  color->green = green * (65535/255);
-  color->blue = blue * (65535/255);
+  color.red   = red   * (65535/255);
+  color.green = green * (65535/255);
+  color.blue  = blue  * (65535/255);
   
   /* the pixel value indicates the index in the colormap of the color.
    * it is simply a combination of the RGB values we set earlier
    */
-  color->pixel = (gulong)(red*65536 + green*256 + blue);
+  color.pixel = (gulong)(red*65536 + green*256 + blue);
 
   /* However, the pixel valule is only truly valid on 24-bit (TrueColor)
    * displays. Therefore, this call is required so that GDK and X can
    * give us the closest color available in the colormap
    */
-  gdk_color_alloc (gtk_widget_get_colormap (widget), color);
+  gdk_color_alloc (gtk_widget_get_colormap (widget), &color);
 
   /* set the foreground to our color */
-  gdk_gc_set_foreground (gc, color);
+  gdk_gc_set_foreground (gc, &color);
 }
 
 
