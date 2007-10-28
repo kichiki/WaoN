da06b9b023ae33bdea45ec5b2af6ec3bd1dbf0ee
kengo ichiki <kengoichiki@gmail.com>
2007-10-28 17:24:20 +0000

    update using "GTK_TYPE_ABOUT_DIALOG".

--- gwaon-about.c
+++ gwaon-about.c
@@ -1,6 +1,6 @@
 /* gWaoN -- gtk+ Spectra Analyzer : about
  * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: gwaon-about.c,v 1.1 2007/02/09 23:03:59 kichiki Exp $
+ * $Id: gwaon-about.c,v 1.2 2007/10/28 17:24:20 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,53 +18,35 @@
  */
 #include <gtk/gtk.h>
 #include <stdlib.h> /* free() */
-
+#include "VERSION.h"
 
 /** global variables **/
 
 
-/** **/
 void
 create_about (void)
 {
-  GtkWidget *window;
-  GtkWidget *vbox;
-  GtkWidget *hbox;
-  GtkWidget *button;
+  GdkPixbuf *logo = gdk_pixbuf_new_from_file ("WaoN.gif", NULL);
 
-  GtkWidget *frame;
-  GtkWidget *label;
+  const gchar *authors[] = { 
+    "Kengo Ichiki <kichiki@users.sourceforge.net>", 
+    NULL 
+  };
 
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_signal_connect (GTK_OBJECT (window), "destroy",
-		      GTK_SIGNAL_FUNC (gtk_widget_destroy),
-		      GTK_OBJECT (window));
-  gtk_widget_set_name (window, "about gWaoN");
-
-  vbox = gtk_vbox_new (FALSE, 5);
-  gtk_container_set_border_width (GTK_CONTAINER (vbox), 10);
-  gtk_container_add (GTK_CONTAINER (window), vbox);
-
-  frame = gtk_frame_new ("gWaoN");
-  label = gtk_label_new ("Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>");
-  gtk_container_add (GTK_CONTAINER (frame), label);
-  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
-
-  frame = gtk_frame_new ("... about gWaoN");
-  label = gtk_label_new ("gWaoN is a gtk+ GUI frontend for WaoN.");
-  gtk_container_add (GTK_CONTAINER (frame), label);
-  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
-
-  frame = gtk_frame_new ("... about WaoN");
-  label = gtk_label_new
-    ("WaoN is a Wave-to-Notes transcriber.\n"
-     "\"WaoN\" also has a meaning of harmony of notes or chord in Japanese!");
-  gtk_container_add (GTK_CONTAINER (frame), label);
-  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
-
-  frame = gtk_frame_new ("GPL");
-  label = gtk_label_new
-    ("This program is free software; you can redistribute it and/or modify "
+  GtkWidget *about = g_object_new
+    (GTK_TYPE_ABOUT_DIALOG,
+     "name", "gWaoN", 
+     "version", WAON_VERSION,
+     "copyright", "(C) 1998-2007 Kengo Ichiki",
+     "comments", "GUI frontend for waon and pv.",
+     "authors", authors,
+     //"documenters", documenters,
+     //"translator-credits", _("translator-credits"),
+     "logo", logo,
+     "website", "http://waon.sourceforge.net/",
+     "website-label", "Visit the WaoN website",
+     "license", "GPL\n\n"
+     "This program is free software; you can redistribute it and/or modify "
      "it under the terms of the GNU General Public License as published by "
      "the Free Software Foundation; either version 2 of the License, or "
      "(at your option) any later version.\n\n"
@@ -74,23 +56,15 @@ create_about (void)
      "GNU General Public License for more details.\n\n"
      "You should have received a copy of the GNU General Public License "
      "along with this program; if not, write to the Free Software "
-     "Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_FILL);
-  gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
-  gtk_container_add (GTK_CONTAINER (frame), label);
-  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
+     "Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.",
+     "wrap-license",       TRUE,
+     NULL);
 
-  /** .. And buttons **/
-  hbox = gtk_hbox_new (FALSE, 5);
-  gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
+  g_object_unref (logo);
 
-  /* quit button */
-  button = gtk_button_new_with_label ("Quit");
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, FALSE, 0);
-  gtk_signal_connect_object (GTK_OBJECT (button), "clicked",
-                             GTK_SIGNAL_FUNC (gtk_widget_destroy),
-                             GTK_OBJECT (window));
+  g_signal_connect (about, "response",
+		    G_CALLBACK (gtk_widget_destroy),
+		    NULL);
 
-
-  gtk_widget_show_all (window);
+  gtk_widget_show_all (about);
 }
